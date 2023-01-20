79049a50037a6bb3feec9c9bd014c7a00519c66e
Loy Mollel <loymollel@msn.com>
2023-01-20 07:25:10 +0300

    Solution

--- separate_duplicates/src/main.rs
+++ separate_duplicates/src/main.rs
@@ -1,5 +1,19 @@
 fn remove_duplicates(input: Vec<i32>) -> i32 {
-    4
+
+    let mut output = 1;
+
+    for (i, x) in input.iter().enumerate() {
+
+        if i < input.len() - 1 {
+            if *x != input[i + 1] {
+                output += 1;
+            }
+        }
+
+    }
+
+    output
+
 }
 
 fn main() {
@@ -19,7 +33,7 @@ mod tests {
     #[test]
     fn test_example_1() {
         let input  = vec![2, 3, 3, 3, 6, 9, 9];
-        assert_eq!(remove_duplicates(input), 5);
+        assert_eq!(remove_duplicates(input), 4);
     }
 
     #[test]
