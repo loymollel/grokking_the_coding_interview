93f5b8d06b1f55fa548b15565262ffa35e63ffdf
Loy Mollel <loymollel@msn.com>
2023-01-24 04:59:01 +0300

    Solution: Removing values in vector

--- Two_Pointers/removing_values/src/main.rs
+++ Two_Pointers/removing_values/src/main.rs
@@ -1,23 +1,33 @@
 fn removing_values(input: Vec<i32>, key: i32) -> i32 {
 
-    for (k,v) in input.iter().enumerate() {
-        // println!("k: {k}, v: {v}");
+    let mut count = 0;
+
+    for (_k, v) in input.iter().enumerate() {
+
+        if v != &key {
+            count += 1;
+        }
     }
+
+    count
 }
 
 
 fn main() {
 
-    let input = vec![3, 2, 3, 6, 3, 10, 9, 3];
-    // let key = 3;
-    //
-    // let output = removing_values(input, key);
-    //
-    // println!("Output: {output}");
+    let input_ex_1 = vec![3, 2, 3, 6, 3, 10, 9, 3];
+    let key_ex_1 = 3;
 
-    // for (k,v) in input.iter().enumerate() {
-    //     println!("k: {k}, v: {v}");
-    // }
+    let output_ex_1 = removing_values(input_ex_1, key_ex_1);
+    println!("Output Example 1 : {output_ex_1}");
+
+
+    let input_ex_2 = vec![2, 11, 2, 2, 1];
+    let key_ex_2 = 2;
+
+    let output_ex_2 = removing_values(input_ex_2, key_ex_2);
+    println!("Output Example 2: {output_ex_2}");
+
 }
 
 #[cfg(test)]
