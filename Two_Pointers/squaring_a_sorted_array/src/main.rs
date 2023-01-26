2e323bb5ea758b3142ecc26d2ab17f035a7d5448
Loy Mollel <loymollel@msn.com>
2023-01-26 07:05:19 +0300

    Solution: Squaring a sorted array

--- Two_Pointers/squaring_a_sorted_array/src/main.rs
+++ Two_Pointers/squaring_a_sorted_array/src/main.rs
@@ -1,5 +1,38 @@
 fn squaring_a_sorted_array(input: Vec<i32>) -> Vec<i32> {
-    vec![0, 1, 4, 4, 9]
+
+    let mut output = vec![0; input.len()];
+
+    let mut left_index = 0;
+    let mut right_index =  input.len() - 1;
+    let mut insertion_index = output.len() - 1;
+
+    while left_index <= right_index {
+
+        let left_square = input[left_index] * input[left_index];
+        let right_square = input[right_index] * input[right_index];
+
+        if left_square > right_square {
+
+            output[insertion_index] = left_square;
+
+            left_index += 1;
+
+        } else {
+
+            output[insertion_index] = right_square;
+
+            right_index -= 1;
+
+        }
+
+        if insertion_index != 0 {
+            insertion_index -= 1;
+        }
+
+    }
+
+    output
+
 }
 
 fn main() {
